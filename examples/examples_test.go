c00997acff18ab9c3126d6664e815e541c81b87c
stack72 <public@paulstack.co.uk>
2020-01-16 15:10:50 +0200

    Adding an acceptance test for Rancher2

--- examples/examples_test.go
+++ examples/examples_test.go
@@ -1,3 +1,44 @@
 // Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
 
 package examples
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/testing/integration"
+)
+
+func TestAccUser(t *testing.T) {
+	test := getJSBaseOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "user"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func getCwd(t *testing.T) string {
+	cwd, err := os.Getwd()
+	if err != nil {
+		t.FailNow()
+	}
+
+	return cwd
+}
+
+func getBaseOptions() integration.ProgramTestOptions {
+	return integration.ProgramTestOptions{}
+}
+
+func getJSBaseOptions(t *testing.T) integration.ProgramTestOptions {
+	base := getBaseOptions()
+	baseJS := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			"@pulumi/rancher2",
+		},
+	})
+
+	return baseJS
+}
