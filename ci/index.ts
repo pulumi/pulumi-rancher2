972ef23f7c16d23e8613e8e4472db25431dc324c
Lee Briggs <lee@leebriggs.co.uk>
2020-05-20 11:38:02 -0700

    fix newline

--- ci/index.ts
+++ ci/index.ts
@@ -80,6 +80,9 @@ const sdkSetupSteps = [
     }
 ]
 
+// https://github.com/jkcfg/jk/issues/182
+const binRunCmd = `find \${{ github.workspace }}/bin -name pulumi-*-\${{ env.PROVIDER }} -print -exec chmod +x {} \\;`
+
 const binarySetupSteps = [
     {
         name: "Download binaries",
@@ -91,7 +94,7 @@ const binarySetupSteps = [
     },
     {
         name: "Restore binary perms",
-        run: "find ${{ github.workspace }}/bin -name 'pulumi-*-${{ env.PROVIDER }}' -print -exec chmod +x {} \;"
+        run: binRunCmd
     }
 ]
 
