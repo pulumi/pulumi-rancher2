3a7a6c0b47804171d00760e794878e34759179d4
Lee Briggs <jaxxstorm@users.noreply.github.com>
2020-05-27 11:56:59 -0700

    Use GitHub Actions for builds (#27)

---
+++ examples/examples_nodejs_test.go
@@ -0,0 +1,32 @@
+// Copyright 2016-2020, Pulumi Corporation.  All rights reserved.
+// +build nodejs all
+
+package examples
+
+import (
+	"path"
+	"testing"
+
+	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
+)
+
+func TestAccUserTs(t *testing.T) {
+	skipIfNoPulumiAccessToken(t)
+	test := getJSBaseOptions().
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "user", "ts"),
+		})
+
+	integration.ProgramTest(t, &test)
+}
+
+func getJSBaseOptions() integration.ProgramTestOptions {
+	base := getBaseOptions()
+	baseJS := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			"@pulumi/rancher2",
+		},
+	})
+
+	return baseJS
+}
--- examples/examples_test.go
+++ examples/examples_test.go
@@ -1,42 +1,11 @@
-// Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
-
 package examples
 
 import (
 	"os"
-	"path"
-	"path/filepath"
 	"testing"
 
 	"github.com/pulumi/pulumi/pkg/v2/testing/integration"
 )
-
-func TestAccUserTs(t *testing.T) {
-	test := getJSBaseOptions().
-		With(integration.ProgramTestOptions{
-			Dir: path.Join(getCwd(t), "user", "ts"),
-		})
-
-	integration.ProgramTest(t, &test)
-}
-
-func TestAccUserPython(t *testing.T) {
-	test := getPythonBaseOptions().
-		With(integration.ProgramTestOptions{
-			Dir: path.Join(getCwd(t), "user", "python"),
-		})
-
-	integration.ProgramTest(t, &test)
-}
-
-func TestAccUserCsharp(t *testing.T) {
-	test := getCSBaseOptions().
-		With(integration.ProgramTestOptions{
-			Dir: path.Join(getCwd(t), "user", "csharp"),
-		})
-
-	integration.ProgramTest(t, &test)
-}
 
 func getCwd(t *testing.T) string {
 	cwd, err := os.Getwd()
@@ -51,35 +20,9 @@ func getBaseOptions() integration.ProgramTestOptions {
 	return integration.ProgramTestOptions{}
 }
 
-func getJSBaseOptions() integration.ProgramTestOptions {
-	base := getBaseOptions()
-	baseJS := base.With(integration.ProgramTestOptions{
-		Dependencies: []string{
-			"@pulumi/rancher2",
-		},
-	})
-
-	return baseJS
-}
-
-func getCSBaseOptions() integration.ProgramTestOptions {
-	base := getBaseOptions()
-	csharpBase := base.With(integration.ProgramTestOptions{
-		Dependencies: []string{
-			"Pulumi.Rancher2",
-		},
-	})
-
-	return csharpBase
-}
-
-func getPythonBaseOptions() integration.ProgramTestOptions {
-	base := getBaseOptions()
-	basePython := base.With(integration.ProgramTestOptions{
-		Dependencies: []string{
-			filepath.Join("..", "sdk", "python", "bin"),
-		},
-	})
-
-	return basePython
+func skipIfNoPulumiAccessToken(t *testing.T) {
+	_, ok := os.LookupEnv("PULUMI_ACCESS_TOKEN")
+	if !ok {
+		t.Skipf("Skipping: PULUMI_ACCESS_TOKEN is not set")
+	}
 }
