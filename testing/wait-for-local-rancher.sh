3d0c179381b3749dea87478e6c6d1ca57346dd88
Lee Briggs <lee@leebriggs.co.uk>
2020-10-20 09:37:06 -0700

    fix shellcheck in script

--- testing/wait-for-local-rancher.sh
+++ testing/wait-for-local-rancher.sh
@@ -7,9 +7,9 @@ HEALTHCHECK_URI="${RANCHER_URL}/ping"
 
 printf "Waiting for local rancher2 to be ready"
 
-until $(curl -k --head --fail --max-time 2 ${HEALTHCHECK_URI}); do
+until curl -k --output /dev/null --silent --head --fail --max-time 2 "${HEALTHCHECK_URI}"; do
     printf '.'
-    printf ${HEALTHCHECK_URI}
+    printf %s "${HEALTHCHECK_URI}"
     sleep 2
 done
 
