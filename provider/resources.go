ea852c11dfd62dd681eb6935f36b4517dac43fe8
Jasmine Dahilig <jasmine@pulumi.com>
2023-03-30 19:47:05 -0700

    add auto token mapping

--- provider/resources.go
+++ provider/resources.go
@@ -23,10 +23,12 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
 	"github.com/pulumi/pulumi-rancher2/provider/v3/pkg/version"
 	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge"
+	"github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfbridge/x"
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	shimv1 "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim/sdk-v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/tokens"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
 	"github.com/rancher/terraform-provider-rancher2/rancher2"
 )
 
@@ -256,6 +258,9 @@ func Provider() tfbridge.ProviderInfo {
 			},
 		})
 
+	defaults := x.TokensSingleModule("rancher2_", mainMod, x.MakeStandardToken(mainPkg))
+	err := x.ComputeDefaults(&prov, defaults)
+	contract.AssertNoErrorf(err, "failed to compute auto token mapping defaults")
 	prov.SetAutonaming(255, "-")
 
 	return prov
